a4ea5d6a028ce7cda814d0b14ca8148ba0e5701c
Aram <aramik16spartak@mail.ru>
2022-04-13 11:41:48 +0300

    Подключение шаблонов

--- www/main.go
+++ www/main.go
@@ -1,7 +1,7 @@
 package main
 
 import ("fmt" //библиотека для вывода (println)
-       "net/http")
+       "net/http"; "html/template")
 
 type Employee struct {
   login string
@@ -18,6 +18,10 @@ func (e Employee) getAllInfo() string {
   e.age)
 }
 
+func (e Employee) setNewPosition(newPosition string)  {
+  e.position = newPosition
+}
+
 func home_page(w http.ResponseWriter, r *http.Request)  {
   aram := Employee{"AAsryan", "Асрян", "Арам", 28, "dev", 9155915671, 13.5}
   fmt.Fprintf(w, "Good job\n" + aram.getAllInfo())
@@ -27,12 +31,20 @@ func about_page(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "About company")
 }
 
+func main_page(w http.ResponseWriter, r *http.Request) {
+  aram := Employee{"AAsryan", "Асрян", "А", 27, "dev", 9155915671, 13.8}
+  tmpl, _ := template.ParseFiles("templates/main.html")
+  tmpl.Execute(w, aram)
+}
+
 func handleRequest() {
   http.HandleFunc("/", home_page)
   http.HandleFunc("/about", about_page)
+  http.HandleFunc("/main", main_page)
   http.ListenAndServe(":8080", nil)
 }
 
+//главная функция
 func main()  {
   handleRequest()
 }
