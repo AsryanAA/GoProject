854a47abdf72380bd47f8faa49d5d9d11c04ec06
Aram <aramik16spartak@mail.ru>
2022-04-13 11:57:27 +0300

    Использование шаблонизатора

--- www/main.go
+++ www/main.go
@@ -4,26 +4,27 @@ import ("fmt" //библиотека для вывода (println)
        "net/http"; "html/template")
 
 type Employee struct {
-  login string
-  surname string
-  name string
-  age uint
-  position string
-  phone int
-  vacation_days float32
+  Login string
+  Surname string
+  Name string
+  Age uint
+  Position string
+  Phone int
+  Vacation_days float32
+  Hobbies [] string
 }
 
 func (e Employee) getAllInfo() string {
-  return fmt.Sprintf("Это : %s %s! Он является : %s. Ему : %d лет", e.surname, e.name, e.position,
-  e.age)
+  return fmt.Sprintf("Это : %s %s! Он является : %s. Ему : %d лет", e.Surname, e.Name, e.Position,
+  e.Age)
 }
 
 func (e Employee) setNewPosition(newPosition string)  {
-  e.position = newPosition
+  e.Position = newPosition
 }
 
 func home_page(w http.ResponseWriter, r *http.Request)  {
-  aram := Employee{"AAsryan", "Асрян", "Арам", 28, "dev", 9155915671, 13.5}
+  aram := Employee{"AAsryan", "Асрян", "Арам", 28, "dev", 9155915671, 13.5, [] string{}}
   fmt.Fprintf(w, "Good job\n" + aram.getAllInfo())
 }
 
@@ -32,7 +33,7 @@ func about_page(w http.ResponseWriter, r *http.Request) {
 }
 
 func main_page(w http.ResponseWriter, r *http.Request) {
-  aram := Employee{"AAsryan", "Асрян", "А", 27, "dev", 9155915671, 13.8}
+  aram := Employee{"AAsryan", "Асрян", "А", 27, "dev", 9155915671, 13.8, [] string{"Football", "Guitar", "Paint"}}
   tmpl, _ := template.ParseFiles("templates/main.html")
   tmpl.Execute(w, aram)
 }
